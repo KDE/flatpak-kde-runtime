5105b077b09134fc6a3cd9ed4301535bba235a7e
Aleix Pol <aleixpol@kde.org>
2017-01-21 02:25:57 +0100

    Simplify

--- generate.sh
+++ generate.sh
@@ -1,10 +1,6 @@
+set -e
+
 make EXPORT_ARGS="--gpg-sign=61C45BED" all export #--generate-static-deltas
-built=$?
-if [ $built -eq 0 ]
-then
-    echo "uploading to darwini.kde.org"
-    rsync -a repo/  distribute@darwini.kde.org:/srv/www/distribute.kde.org/flatpak-testing/
-else
-    echo "didn't upload"
-fi
- 
+
+echo "uploading to darwini.kde.org"
+rsync -a repo/  distribute@darwini.kde.org:/srv/www/distribute.kde.org/flatpak-testing/
