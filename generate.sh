afa9bf8fb21fdbccefe3fd817a26cdc13810a6c6
Aleix Pol <aleixpol@kde.org>
2017-02-14 16:14:36 +0100

    Adopt tweaks that ease the execution of the script over ssh

--- generate.sh
+++ generate.sh
@@ -1,6 +1,11 @@
 set -e
 
-make EXPORT_ARGS="--gpg-sign=61C45BED" all export #--generate-static-deltas
+make EXPORT_ARGS="--gpg-sign=61C45BED" all #--generate-static-deltas
+
+# make sure op is in front of the computer, otherwise it times out
+read -p "Press enter to continue"
+
+make EXPORT_ARGS="--gpg-sign=61C45BED" export #--generate-static-deltas
 
 echo "uploading to darwini.kde.org"
 rsync -a repo/  distribute@darwini.kde.org:/srv/www/distribute.kde.org/flatpak-testing/
