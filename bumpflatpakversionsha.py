eb69a9891293415f2a8cc8977b257b58e5545568
Aleix Pol <aleixpol@kde.org>
2022-05-12 20:35:24 +0200

    Qt 6.3.0

--- bumpflatpakversionsha.py
+++ bumpflatpakversionsha.py
@@ -182,7 +182,7 @@ def processModule(module):
         for source in module['sources']:
             try:
                 checkGitHubRepository(source, replace)
-                #checkArchiveSha256(source, replace)
+                checkArchiveSha256(source, replace)
                 checkGitNextTag(source, replace)
                 checkPythonHosted(source, replace)
                 checkKDEQtPatchCollection(source, replace)
