dcf716ed2df2e0ef1d29f50137e4dbc2d03196f2
Aleix Pol <aleixpol@kde.org>
2022-07-28 12:58:47 +0200

    Now kate complains about python coding style...

--- bumpflatpakversionsha.py
+++ bumpflatpakversionsha.py
@@ -1,6 +1,6 @@
 #
 # Copyright 2018  Aleix Pol Gonzalez <aleixpol@kde.org>
-# 
+#
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
 # published by the Free Software Foundation; either version 2 of
@@ -8,12 +8,12 @@
 # accepted by the membership of KDE e.V. (or its successor approved
 # by the membership of KDE e.V.), which shall act as a proxy
 # defined in Section 14 of version 3 of the license.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
@@ -27,6 +27,7 @@ import os
 import git
 import re
 from urllib.parse import urlparse
+
 
 def calculate_sha256(url):
     urlsha = url + ".sha256"
@@ -53,12 +54,14 @@ def calculate_sha256(url):
         sha256.update(data)
     return sha256.hexdigest()
 
+
 def checkArchiveSha256(source, replace):
     if source['type'] == 'archive':
         sha = calculate_sha256(source['url'])
         if sha and sha != source['sha256']:
             print("new sha", source, sha)
             replace[source['sha256']] = sha
+
 
 def checkRepo(url):
     directory = "./check/" + os.path.basename(urlparse(url).path)
@@ -69,6 +72,7 @@ def checkRepo(url):
         repo = git.Repo()
         repo.clone_from(url, directory)
     return repo
+
 
 def checkGitNextTag(source, replace):
     if source['type'] == 'git' and ('branch' in source or 'tag' in source):
@@ -86,7 +90,7 @@ def checkGitNextTag(source, replace):
 
         if branch in repo.tags:
             usedCommit = repo.commit(repo.tags[branch].object)
-            found = usedCommit != None
+            found = usedCommit is not None
             for tag in repo.tags:
                 tagCommit = repo.commit(tag.object)
                 if 'rc' in tag.name or 'beta' in tag.name or 'alpha' in tag.name:
@@ -100,9 +104,9 @@ def checkGitNextTag(source, replace):
                 if ref.name == 'origin/' + branch:
                     usedCommit = ref.commit
 
-            found = usedCommit != None
+            found = usedCommit is not None
             if found:
-                toignore = [ 'origin/master', 'origin/HEAD' ]
+                toignore = ['origin/master', 'origin/HEAD']
                 for otherRef in repo.remotes.origin.refs:
                     if otherRef.name in toignore or otherRef.name.startswith('origin/work/'):
                         continue
@@ -113,6 +117,7 @@ def checkGitNextTag(source, replace):
         if not found:
             print("wtf", source['url'], source['branch'])
 
+
 def checkGitHubRepository(source, replace):
     if source['type'] == 'archive' and source['url'].startswith("https://github.com/"):
         url = source['url']
@@ -121,12 +126,13 @@ def checkGitHubRepository(source, replace):
             m = re.search('https://github.com/(.+)/(.+)/archive/(.*).tar.gz', url)
 
         if m:
-            checkGitNextTag({'type': 'git', 'url': 'https://github.com/' + m.group(1) + '/' + m.group(2) + '.git', 'tag': m.group(3) }, {})
+            checkGitNextTag({'type': 'git', 'url': 'https://github.com/' + m.group(1) + '/' + m.group(2) + '.git', 'tag': m.group(3)}, {})
         else:
             print("could not recognize", url)
 
+
 def checkPythonHosted(source, replace):
-    if not 'url' in source:
+    if 'url' not in source:
         return
     pythonHosted = source['type'] == 'archive' and source['url'].startswith('https://files.pythonhosted')
     pipy = source['type'] == 'file' and source['url'].startswith('https://pypi.python.org')
@@ -134,7 +140,7 @@ def checkPythonHosted(source, replace):
     if pythonHosted or pipy:
         name = os.path.basename(urlparse(source['url']).path)
         m = re.search('(.+)-[0-9]', name)
-        pkgname=m.group(1)
+        pkgname = m.group(1)
         r = requests.get('https://pypi.org/pypi/' + pkgname + '/json')
 
         content = json.loads(r.text)
@@ -147,6 +153,7 @@ def checkPythonHosted(source, replace):
             if source['url'].endswith('.whl') and asset["packagetype"] == 'bdist_wheel':
                 if asset['url'] != source['url']:
                     print("new version of", pkgname, json.dumps({'type': 'file', 'url': asset['url'], 'sha256': asset['digests']['sha256']}))
+
 
 def checkKDEQtPatchCollection(source, replace):
     if source['type'] == 'git' and source['url'].startswith("https://invent.kde.org/qt/qt/"):
@@ -163,33 +170,35 @@ def checkKDEQtPatchCollection(source, replace):
         else:
             print("No branch %s for %s" % (branchName, source['url']))
 
+
 def processModule(module):
     if isinstance(module, str):
         with open(module, 'r') as moduleFile:
             content = moduleFile.read()
         try:
             value = json.loads(content)
-        except:
-            print("failed to parse", x)
+        except Exception as err:
+            print("failed to parse", x, err)
             return {}
-        return processModule(json.loads(content))
+        return processModule(value)
 
     replace = {}
     if 'sources' in module:
         for source in module['sources']:
             try:
                 checkGitHubRepository(source, replace)
-                #checkArchiveSha256(source, replace)
+                # checkArchiveSha256(source, replace)
                 checkGitNextTag(source, replace)
                 checkPythonHosted(source, replace)
                 checkKDEQtPatchCollection(source, replace)
-            except:
-                print("Failed processing", source)
+            except Exception as err:
+                print("Failed processing", source, err)
 
     if 'modules' in module and isinstance(module['modules'], list):
         for submodule in module['modules']:
             replace = {**replace, **processModule(submodule)}
     return replace
+
 
 if __name__ == "__main__":
     content = ""
@@ -202,7 +211,6 @@ if __name__ == "__main__":
         except:
             print("failed to parse", x)
             continue
-
 
         pool = multiprocessing.Pool(6)
         replacements = pool.map(processModule, value['modules'])
