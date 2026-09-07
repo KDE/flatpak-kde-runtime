735ee8158312c70ad9e2e6894803a8ff2ccf2dec
Aleix Pol i Gonzalez <aleixpol@kde.org>
2026-09-07 13:07:06 +0200

    cxx-rust-cssparser: Fix builds
    And adopt tags for the repository as they do not require master anymore.

--- refresh-cargo-deps.sh
+++ refresh-cargo-deps.sh
@@ -2,16 +2,20 @@
 
 set -eu
 
-CARGO_FILE=`cat org.kde.Sdk.json.in | jq -r '
+CARGO_FILE=$(jq -r '
   .. | objects | select(.name? == "cxx-rust-cssparser") 
   | .sources[] | select(.type? == "git") 
   | "\(.url)/-/raw/\(.tag // .commit // .branch // "master")/rust/Cargo.lock"
-'`
+' org.kde.Sdk.json.in)
 
 # something like https://invent.kde.org/libraries/cxx-rust-cssparser/-/raw/v1.0.0/rust/Cargo.lock
 wget $CARGO_FILE
 wget https://raw.githubusercontent.com/flatpak/flatpak-builder-tools/refs/heads/master/cargo/flatpak-cargo-generator.py
 
 python flatpak-cargo-generator.py Cargo.lock -o cxx-rust-cssparser-generated-sources.json
+
+sed -i -e 's/"dest": "cargo"/"dest": ".cargo"/' \
+  -e 's/"dest-filename": "config"/"dest-filename": "config.toml"/' \
+  cxx-rust-cssparser-generated-sources.json
 
 rm Cargo.lock flatpak-cargo-generator.py
